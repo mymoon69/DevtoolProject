ac0d4fb3dd7311df6a6c87aa4b653de0ef5725fc
whiskas69 <64070075@it.kmail.ac.th>
2024-03-07 22:02:57 +0700

    list your trips

---
+++ travel/src/screen/listConfirm.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <header>
+        <i class="fa fa-bars"></i>
+        <label class="text-2xl">List You Trip</label>
+    </header>
+
+    <div class="flex  rounded-lg shadow-lg p-5 items-center">
+      <div class="w-1/3 ">
+        <img
+          class="loginImg w-[100px] h-[120px] bg-cover rounded-lg"
+          src="https://cdn.discordapp.com/attachments/979676845228318740/1018546182768836648/Imagenes_de_Coco_-_Imector.jpeg?ex=65f70e8d&is=65e4998d&hm=780f2ffa8091385150b6dcd4ddf8b7b8ee535ea47fafde74d6cb7cab6e30a859&"
+          alt="coverBook"
+        />
+      </div>
+
+      <div class="w-2/3 p-4 md:p-4 ">
+        <h1 class="text-l font-bold text-gray-800 ">
+          Backpack
+        </h1>
+
+        <div class="star flex mt-2 item-center">
+          <svg
+            class="w-5 h-5 text-yellow-400 fill-current "
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+            />
+          </svg>
+
+          <svg
+            class="w-5 h-5 text-yellow-400 fill-current "
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+            />
+          </svg>
+
+          <svg
+            class="w-5 h-5 text-yellow-400 fill-current "
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+            />
+          </svg>
+
+          <svg class="w-5 h-5 text-yellow-400 fill-current" viewBox="0 0 24 24">
+            <path
+              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+            />
+          </svg>
+
+          <svg class="w-5 h-5 text-gray-500 fill-current" viewBox="0 0 24 24">
+            <path
+              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+            />
+          </svg>
+          <p class="text-s  ">
+            100 reviews
+        </p>
+        </div>
+
+        <p class="mt-2 text-xs text-gray-600 ">
+          Lorem ipsum dolor 
+        </p>
+
+        
+
+        <div class="flex justify-between mt-3 item-center">
+          <h1
+            class="text-lg font-bold text-gray-700  md:text-xl"
+          >
+            $220 or 2 days 1 night
+          </h1>
+          <!-- <button
+            class="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600"
+          >
+            Add to Cart
+          </button> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ travel/src/screen/listYourTrips.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header>
+      <i class="fa fa-bars"></i>
+      <label class="text-2xl">List You Trip</label>
+      <div class="search flex flex-row bg-blue-300 w-full">
+        <input
+          id="search"
+          name="search"
+          type="search"
+          autocomplete="search"
+          class="block border-solid border-2 border-neutral-400 w-auto p-2 ps-3 text-sm text-camelcoat rounded-full"
+        />
+        <button>ff</button>
+      </div>
+    </header>
+    <div
+      class="trip items-center box-content p-5 mt-[30px] border-solid border-2 border-neutral-400 rounded-[30px] drop-shadow-lg"
+    >
+      <div>
+        <label class="name">Book Name Der</label>
+      </div>
+      <div>
+        <label class="price text-orange-600">$ 1233333</label>
+      </div>
+      <div>
+        <i class="fa fa-calendar"></i>
+        <label class="date text-gray-400">10-01-1010 - 10-08-1010</label>
+      </div>
+    </div>
+  </div>
+</template>
